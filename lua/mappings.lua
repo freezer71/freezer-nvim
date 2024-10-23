1d4ba2705e1357e55042cbc8b60dc16b9728f174
freezer71 <tharsan77340@gmail.com>
2024-10-23 03:59:42 +0200

    first commit

---
+++ lua/mappings.lua
@@ -0,0 +1,19 @@
+local map = vim.keymap.set
+
+
+map("n", ";", ":", { desc = "CMD enter command mode" })
+map("i", "jk", "<ESC>")
+
+map("n", "<C-q>", ":NvimTreeFocus<CR>")
+map("n", "<C-b>", ":NvimTreeToggle<CR>")
+
+map("t","<ESC>","<C-\\><C-n>")
+
+map("n","<C-e>",":Telescope find_files<CR>")
+
+map("","<Tab>",":BufferLineCycleNext<CR>")
+map("","<S-Tab>",":BufferLineCyclePrev<CR>")
+map("","<C-F>",":Neoformat<CR>")
+
+map("n","<A-j>",":m +1<CR>")
+map("n","<A-k>",":m -2<CR>")
---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,31 @@
+return {
+    'nvim-telescope/telescope.nvim', tag = '0.1.8',
+      dependencies = {
+          'nvim-lua/plenary.nvim',
+          { 'nvim-telescope/telescope-fzf-native.nvim', build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release' },
+          "nvim-tree/nvim-web-devicons"
+      },
+      config = function()
+        local telescope = require("telescope")
+        telescope.setup({
+            defaults = {
+                file_ignore_patterns = {
+                    "node_modules",
+                    ".git",
+                    "dist",
+                    "build",
+                    ".idea"
+                }
+            },
+             mappings = {
+                i = {
+                    ["<Enter>"] = "file_tab",
+                },
+                n = {
+                    ["<Enter>"] = "file_vsplit",
+                }
+            },
+        })
+        telescope.load_extension("fzf")
+      end
+}
