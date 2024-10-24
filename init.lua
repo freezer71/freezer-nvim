0cace33647b86e0b8dbfd854c56f2d3fe713e235
freezer71 <tharsan77340@gmail.com>
2024-10-24 14:29:47 +0200

    [feat] adding goconfig to lps

--- init.lua
+++ init.lua
@@ -6,3 +6,5 @@ vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a T
 vim.o.softtabstop = 2 -- Number of spaces inserted instead of a TAB character
 vim.o.shiftwidth = 2 -- Number of spaces inserted when indenting
 
+vim.wo.relativenumber = true
+vim.wo.number = true
