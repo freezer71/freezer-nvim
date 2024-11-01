7dec6708b99c3230c8a9bdcc136bbdb66d8cc539
freezer71 <tharsan77340@gmail.com>
2024-11-01 23:30:44 +0100

    [feat] adding telescop extensition

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,7 +2,7 @@ return {
     'nvim-telescope/telescope.nvim', tag = '0.1.8',
       dependencies = {
           'nvim-lua/plenary.nvim',
-          { 'nvim-telescope/telescope-fzf-native.nvim', build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release' },
+          { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
           "nvim-tree/nvim-web-devicons"
       },
       config = function()
