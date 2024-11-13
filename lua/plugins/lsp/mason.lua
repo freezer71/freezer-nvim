2f25730a8c40a84ac63621b25225fd132f72beed
freezer71 <tharsan77340@gmail.com>
2024-11-13 22:47:46 +0100

    [feat] remove java lsp server

--- lua/plugins/lsp/mason.lua
+++ lua/plugins/lsp/mason.lua
@@ -1,37 +1,36 @@
 return {
-  "williamboman/mason.nvim",
-  dependencies = {
-    "williamboman/mason-lspconfig.nvim",
-  },
-  config = function()
-    local mason = require("mason")
-    local mason_lspconfig = require("mason-lspconfig")
+	"williamboman/mason.nvim",
+	dependencies = {
+		"williamboman/mason-lspconfig.nvim",
+	},
+	config = function()
+		local mason = require("mason")
+		local mason_lspconfig = require("mason-lspconfig")
 
-    mason.setup({
-      ui = {
-        icons = {
-          package_installed = "✓",
-          package_pending = "➜",
-          package_uninstalled = "✗",
-        },
-      },
-    })
-    mason_lspconfig.setup({
-      ensure_installed = {
-        "html",
-        "cssls",
-        "tailwindcss",
-        "lua_ls",
-        "emmet_ls",
-        "prismals",
-        "pyright",
-        "ts_ls",
-        "gopls",
-        "rust_analyzer",
-        "jdtls",
-        "java_language_server"
-      },
-      automatic_installation = true,
-    })
-  end,
+		mason.setup({
+			ui = {
+				icons = {
+					package_installed = "✓",
+					package_pending = "➜",
+					package_uninstalled = "✗",
+				},
+			},
+		})
+		mason_lspconfig.setup({
+			ensure_installed = {
+				"html",
+				"cssls",
+				"tailwindcss",
+				"lua_ls",
+				"emmet_ls",
+				"prismals",
+				"pyright",
+				"ts_ls",
+				"gopls",
+				"rust_analyzer",
+				"jdtls",
+			},
+			automatic_installation = true,
+		})
+	end,
 }
